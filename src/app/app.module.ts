5704fab2fdc4a5661a09bac62da70e35b2337a1f
Maria660253036 <mariastarpower@gmail.com>
2018-06-17 17:49:53 -0700

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { OrderPage } from '../pages/order/order';
+import { MyComponent } from '../components/foo';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    OrderPage,
+    MyComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    OrderPage,
+    MyComponent
   ],
   providers: [
     StatusBar,
